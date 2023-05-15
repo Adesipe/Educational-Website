a00923521bf7c21e5df1b069bb2d536ed3809008
Adesipe <sodiqadesipe13@gmail.com>
2023-05-15 17:18:52 +0100

    commit

--- JS/script.js
+++ JS/script.js
@@ -17,7 +17,7 @@ let sideBar = document.querySelector('.side-bar');
 document.querySelector('#menu-btn').onclick = () => {
     sideBar.classList.toggle('active');
     body.classList.toggle('active');
-}
+};
 
 document.querySelector('.side-bar .close-side-bar').onclick = () =>{
     sideBar.classList.remove('active');
